ead406852892e4e7154ed208e63539358f94e8df
VictorKrchv <60627020+VictorKrchv@users.noreply.github.com>
2020-03-11 21:39:22 +0200

    Update FormsControls.jsx

--- src/components/common/FormsControls/FormsControls.jsx
+++ src/components/common/FormsControls/FormsControls.jsx
@@ -5,6 +5,7 @@ import s from './FormsControls.module.css';
 export const Element = Element => ({ input, meta, ...props }) => {
     const hasError = meta.touched && meta.error;
     return (
+        123
       <div className={ s.formControl + " " + (hasError ? s.error : "") }>
         <Element {...input} {...props} />
         { hasError && <span> { meta.error } </span> }
