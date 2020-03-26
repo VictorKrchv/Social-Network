3cddb064f710b3344f48b4a1a92a4ac552320eb9
VictorKrchv <bublikoo228@gmail.com>
2020-03-27 00:05:10 +0200

    add message

--- src/components/Dialogs/Message/Message.jsx
+++ src/components/Dialogs/Message/Message.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import s from './../Dialogs.module.css';
 
+
 const Message = (props) => {
+
+    let stl = props.userId == props.senderId ? `${s.message_bg }` : `${s.message_bg} ${s.incoming}`
+
+
     return <div className={s.message}>
-        <div className={s.message__bg}>{props.message}</div>
+        <div className={`${stl}`}>{props.message}</div>
+        <div className={s.message__delete}><button onClick={props.deleteMessage.bind(null, props.id)}>delete</button></div>
     </div>
 }
 
